a634946a461d872be0981c4b196d982cc3b75ebc
urk21cs2021JOSHUA <109647940+urk21cs2021JOSHUA@users.noreply.github.com>
2022-09-28 09:57:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
+  # Write your code here
   for i in range(1, len(array)):
         k = array[i]
         j = i-1
@@ -9,16 +10,6 @@ def insertionSort(array) -> List[int]:
                 j -= 1
         array[j + 1] = k
   return array  
-
-# data = [9, 5, 1, 4, 3]
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(insertionSort(data))
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
